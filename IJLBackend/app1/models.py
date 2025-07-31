8afb19927c5cf2b77dbff2a9666e2da62b059ea7
jeeshmaNL <jeeshma.s@vbsai.com>
2025-07-31 21:33:33 +0530

    import os

--- IJLBackend/app1/models.py
+++ IJLBackend/app1/models.py
@@ -9,6 +9,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.db.models import Sum
+from os import path
 
 # ==========================
 # Device Management Modelss
